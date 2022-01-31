e83db4f64c2b3f9a4d3dec990773232fdcf17faa
adShoheiTerashima <s-terashima@adventure-inc.co.jp>
2022-01-31 21:54:33 +0900

    feat(front): add algolia

--- frontend/components/searchMenu/index.tsx
+++ frontend/components/searchMenu/index.tsx
@@ -1,5 +1,7 @@
 import { useContext, useEffect, useRef, useState } from 'react'
 import { useRouter } from 'next/router'
+import { InstantSearch, Hits, Configure } from 'react-instantsearch-dom'
+import algoliasearch from 'algoliasearch/lite'
 
 import { SearchConditionContext } from '@lib/hooks/useSearchCondition'
 import { FORM_TYPE } from '@lib/utils/const'
@@ -7,7 +9,7 @@ import { focusFormType } from '@lib/utils/types'
 
 import SubmitButton from '@components/searchMenu/SubmitButton'
 import SearchInput from '@components/searchMenu/Input'
-import SuggestList from '@components/searchMenu/SuggestList'
+import SuggestItem from '@components/searchMenu/SuggestItem'
 import GuestCount from '@components/searchMenu/GuestCount'
 
 type Props = {
@@ -21,6 +23,12 @@ const SearchForm = ({ focusForm, setForcusForm }: Props) => {
   const searchConditionCtx = useContext(SearchConditionContext)
   const [guests, setGuests] = useState(0)
   const router = useRouter()
+
+  const searchClient = algoliasearch(
+    process.env.NEXT_PUBLIC_ALGOLIA_APPLICATION_ID || '',
+    process.env.NEXT_PUBLIC_ALGOLIA_SEARCH_API_KEY || '',
+  )
+  const indexName = process.env.NEXT_PUBLIC_ALGOLIA_INDEX || ''
 
   // 検索テキストフォーム周り
   const [inputCity, setInputCity] = useState(searchConditionCtx.city)
@@ -84,59 +92,70 @@ const SearchForm = ({ focusForm, setForcusForm }: Props) => {
 
   return (
     <div className="absolute top-0 h-115 w-full bg-white p-24 pb-0">
-      <div className="grid grid-cols-3 rounded-2xl border border-gray-F2F2F2 font-Mulish shadow-search">
-        <div className="flex">
-          <div
-            className="focus:outline-none h-full w-full cursor-pointer rounded-2xl border border-white p-2.5 focus-within:border-black hover:border-black"
-            ref={locationRef}
-            onClick={clickLocation}
-            tabIndex={0}
-          >
-            <SearchInput
-              id="search-location"
-              label="LOCATION"
-              placeholder="Add location"
-              inputText={inputText}
-              change={changeText}
-            />
+      <InstantSearch indexName={indexName} searchClient={searchClient}>
+        <Configure hitsPerPage={4} />
+        <div className="grid grid-cols-3 rounded-2xl border border-gray-F2F2F2 font-Mulish shadow-search">
+          <div className="flex">
+            <div
+              className="focus:outline-none h-full w-full cursor-pointer rounded-2xl border border-white p-2.5 focus-within:border-black hover:border-black"
+              ref={locationRef}
+              onClick={clickLocation}
+              tabIndex={0}
+            >
+              <SearchInput
+                id="search-location"
+                label="LOCATION"
+                placeholder="Add location"
+                inputText={inputText}
+                change={changeText}
+              />
+            </div>
+            <div className="border-l border-gray-F2F2F2" />
           </div>
-          <div className="border-l border-gray-F2F2F2" />
+          <div className="flex">
+            <div
+              className="focus:outline-none h-full w-full  cursor-pointer rounded-2xl border border-white  p-2.5 hover:border-black focus:border-black"
+              ref={guestsRef}
+              onClick={clickGuests}
+              tabIndex={0}
+            >
+              <p className="mb-1 text-0.5 font-extrabold">GUESTS</p>
+              {isZeroGuest ? (
+                <p className="text-sm text-gray-BDBDBD">Add guests</p>
+              ) : (
+                <p className="text-sm">{guests} guests</p>
+              )}
+            </div>
+            <div className="border-l border-gray-F2F2F2" />
+          </div>
+          <div className="flex items-center justify-center">
+            <SubmitButton click={clickSubmitButton} />
+          </div>
         </div>
-        <div className="flex">
-          <div
-            className="focus:outline-none h-full w-full  cursor-pointer rounded-2xl border border-white  p-2.5 hover:border-black focus:border-black"
-            ref={guestsRef}
-            onClick={clickGuests}
-            tabIndex={0}
-          >
-            <p className="mb-1 text-0.5 font-extrabold">GUESTS</p>
-            {isZeroGuest ? (
-              <p className="text-sm text-gray-BDBDBD">Add guests</p>
+        <div className="mt-11 grid grid-cols-3">
+          <div className="ml-1.5">
+            {isFocusLocation ? (
+              <ul>
+                <Hits hitComponent={SuggestItem} />
+              </ul>
             ) : (
-              <p className="text-sm">{guests} guests</p>
+              ''
             )}
           </div>
-          <div className="border-l border-gray-F2F2F2" />
+          <div className="ml-2.5">
+            {isFocusGuests ? (
+              <GuestCount
+                countAdults={countAdults}
+                countChildren={countChildren}
+                inputAdults={inputAdults}
+                inputChildren={inputChildren}
+              />
+            ) : (
+              ''
+            )}
+          </div>
         </div>
-        <div className="flex items-center justify-center">
-          <SubmitButton click={clickSubmitButton} />
-        </div>
-      </div>
-      <div className="mt-11 grid grid-cols-3">
-        <div className="ml-1.5">{isFocusLocation ? <SuggestList click={clickSuggest} /> : ''}</div>
-        <div className="ml-2.5">
-          {isFocusGuests ? (
-            <GuestCount
-              countAdults={countAdults}
-              countChildren={countChildren}
-              inputAdults={inputAdults}
-              inputChildren={inputChildren}
-            />
-          ) : (
-            ''
-          )}
-        </div>
-      </div>
+      </InstantSearch>
     </div>
   )
 }
