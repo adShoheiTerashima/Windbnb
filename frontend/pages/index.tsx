0907ba19a8a6c7265bc6196815bc60bad3bde9c7
adShoheiTerashima <s-terashima@adventure-inc.co.jp>
2022-02-02 23:40:22 +0900

    feat(front): algolia search test

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,4 +1,5 @@
 import type { GetServerSideProps, NextPage } from 'next'
+import algoliasearch from 'algoliasearch/lite'
 
 import {
   useSearchCondition,
@@ -11,12 +12,43 @@ import Page from '@components/common/Page'
 import Layout from '@components/common/Layout'
 import PropertyList from '@components/propertyList'
 
+type searchResult = {
+  city: string
+  country: string
+  superHost: boolean
+  title: string
+  rating: number
+  maxGuests: number
+  type: string
+  beds: number
+  photo: string
+}
+
 type Props = {
   data: defaultSearchConditionType
+  list: searchResult[]
 }
 
 export const getServerSideProps: GetServerSideProps<Props> = async (context) => {
-  await _sleep(200) // dummy
+  // とりあえずalgoliaどう動かすのかテスト
+  // いったん適当に書き殴ってみる
+  const ALGOLIA_APP_ID = process.env.NEXT_PUBLIC_ALGOLIA_APPLICATION_ID || ''
+  const ALGOLIA_API_KEY = process.env.NEXT_PUBLIC_ALGOLIA_SEARCH_ONLY_API_KEY || ''
+  const ALGOLIA_INDEX_NAME = process.env.NEXT_PUBLIC_ALGOLIA_INDEX || ''
+  const index = algoliasearch(ALGOLIA_APP_ID, ALGOLIA_API_KEY).initIndex(ALGOLIA_INDEX_NAME)
+  const filters: string[] = []
+  if (isString(context.query.city) && context.query.city !== '') {
+    filters.push(`(city:"${context.query.city}")`)
+  }
+  if (isString(context.query.country) && context.query.country !== '') {
+    filters.push(`(country:"${context.query.country}")`)
+  }
+  if (isNumber(context.query.adults) && isNumber(context.query.children)) {
+    filters.push(`(maxGuests:${context.query.adults + context.query.children})`)
+  }
+  const t = filters.join(' AND ')
+  const { hits }: { hits: searchResult[] } = await index.search('', { filters: t })
+
   return {
     props: {
       data: {
@@ -25,27 +57,25 @@ export const getServerSideProps: GetServerSideProps<Props> = async (context) => 
         adults: isNumber(context.query.adults) ? +context.query.adults : 0,
         children: isNumber(context.query.children) ? +context.query.children : 0,
       },
+      list: hits,
     },
   }
 }
 
-const Home: NextPage<Props> = ({ data }: Props) => {
+const Home: NextPage<Props> = ({ data, list }: Props) => {
   const meta = {
     title: '一覧画面 - Windbnb',
     description: 'Windbnbの一覧画面です。',
     noindex: true,
   }
 
-  // [TODO]実際のアプリケーションではSEOを考えてserverPropsで取得するはず
-  // 今回もその予定
-  console.log(data)
   const SearchConditionCtx = useSearchCondition(data)
 
   return (
     <Page meta={meta}>
       <SearchConditionContext.Provider value={SearchConditionCtx}>
         <Layout>
-          <PropertyList />
+          <PropertyList list={list} />
         </Layout>
       </SearchConditionContext.Provider>
     </Page>
