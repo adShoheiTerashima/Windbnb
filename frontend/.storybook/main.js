7a79156a187601e98e0f838dc8270c6887e716aa
adShoheiTerashima <s-terashima@adventure-inc.co.jp>
2022-02-08 23:33:11 +0900

    feat(front): storybook setup

--- frontend/.storybook/main.js
+++ frontend/.storybook/main.js
@@ -1,10 +1,34 @@
+const path = require('path')
 module.exports = {
-  "stories": [
-    "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
+  webpackFinal: async (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@components': path.resolve(__dirname, '../components'),
+      '@lib': path.resolve(__dirname, '../lib'),
+      '@domain': path.resolve(__dirname, '../domain'),
+    }
+    return {
+      ...config,
+      resolve: {
+        ...config.resolve,
+        alias: {
+          ...config.resolve.alias,
+          'next-i18next': 'react-i18next',
+        },
+      },
+    }
+  },
+  stories: ['../components/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    {
+      name: '@storybook/addon-postcss',
+      options: {
+        postcssLoaderOptions: {
+          implementation: require('postcss'),
+        },
+      },
+    },
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials"
-  ]
 }
